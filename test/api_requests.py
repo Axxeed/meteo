c4cb5dd024388de5cc7b445f21eed7de4ceb7c25
NaNais <merien.nais@gmail.com>
2023-08-31 14:00:40 +0200

    response error 200

--- test/api_requests.py
+++ test/api_requests.py
@@ -8,18 +8,25 @@ params = {
 }
 
 response = requests.get(url, params=params)
-data = response.json()
-print(data)
+if response.status_code == 200:
+    data = response.json()
+    print(data)
+    url_weather = "https://api.openweathermap.org/data/2.5/weather"
 
-url_weather = "https://api.openweathermap.org/data/2.5/weather"
+    latitude = data[0].get("lat")
+    longitude = data[0].get("lon")
 
-params_weather = {
-    "lat": "xxxxxxxx",
-    "lon": "xxxxxxx",
-    "appid": "XXXXXXXXXX"
-}
+    params_weather = {
+        "lat": latitude,
+        "lon": longitude,
+        "appid": api_key
+    }
 
-response_weather = requests.get(url_weather, params=params_weather)
-data_weather = response_weather.json()
-
-print(data_weather)
+    response_weather = requests.get(url_weather, params=params_weather)
+    if response_weather.status_code == 200 :
+        data_weather = response_weather.json()
+        print(data_weather)
+    else :
+        print(f"Error:{response_weather.status_code}")
+else :
+    print(f"Error:{response.status_code}")
